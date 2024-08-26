084d7289a10a8d56f04517211bb7f7ae817b0553
Thomas <thomascode92@hotmail.com>
2024-08-26 08:00:06 +0200

    Demo Project Setup & A Dockerfile

--- node-app/data/database.js
+++ node-app/data/database.js
@@ -1,11 +1,12 @@
 import { MongoClient } from 'mongodb';
 
+const connectionProtocol = process.env.MONGODB_CONNECTION_PROTOCOL;
 const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
@@ -18,6 +19,7 @@ try {
   console.log('Connection failed.');
   await client.close();
   console.log('Connection closed.');
+  process.exit(1);
 }
 
 const database = client.db(dbName);
