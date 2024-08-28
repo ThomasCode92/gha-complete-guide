655df00bda99cd238de9a3b818d07a05baa91e28
Thomas <thomascode92@hotmail.com>
2024-08-28 08:46:08 +0200

    Adding Action Inputs

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,8 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+  // get input values
+
   core.notice('Hello form a custom JavaScript action!');
 }
 
