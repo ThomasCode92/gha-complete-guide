bdace6ab5fea4cbc7359519c5f1a8da0011d74d9
Thomas <thomascode92@hotmail.com>
2024-08-28 09:25:44 +0200

    Enable Dry-Run for Deployment

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -6,12 +6,26 @@ function run() {
   const bucket = core.getInput('bucket', { required: true });
   const bucketRegion = core.getInput('bucket-region', { required: true });
   const distFolder = core.getInput('dist-folder', { required: true });
+  const dryRun = core.getInput('dry-run', { required: true });
+
+  const s3Uri = `s3://${bucket}`;
+  const execCommand = `aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion} --delete`;
+
+  // dry run, skip deployment
+  if (dryRun === 'true') {
+    core.notice('Dry run enabled, skipping deployment');
+    core.notice('The following command would have been executed:');
+    core.notice(execCommand);
+
+    return;
+  }
 
   // upload files
-  const s3Uri = `s3://${bucket}`;
-  exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion} --delete`);
+  core.notice('Uploading files to S3...');
+  core.notice('The following command is being executed:');
+  core.notice(execCommand);
 
-  core.notice('Hello form a custom JavaScript action!');
+  exec(execCommand);
 }
 
 run();
