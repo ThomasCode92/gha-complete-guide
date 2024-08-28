334eb648b15ddc79c77162ef489700d0d185c44b
Thomas <thomascode92@hotmail.com>
2024-08-28 08:54:52 +0200

    Interacting with GitHub Actions Features

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,9 +1,15 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
-const exec = require('@actions/exec');
+const { exec } = require('@actions/exec');
 
 function run() {
   // get input values
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // upload files
+  const s3Uri = `s3://${bucket}`;
+  exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion} --delete`);
 
   core.notice('Hello form a custom JavaScript action!');
 }
